ccba145a3461fe7c77c3c3795c22de1c6b2ade5d
Charawey-X <charawey@gmail.com>
2022-12-08 12:20:05 +0300

    add home content

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,14 +6,20 @@ import home from "../../assets/home.jpg";
 class Home extends React.Component {
   render() {
 
-    const content = "";
+    const content = "Need a cocktail recipe?\nSearch for a drink by name, or ingredient,\nand we'll show you what you need to make it.\nYou can make signature cocktails right from your home,\n and save your favorites for later.";
 
     return (
       <div>
          <h1>THE VIRTUAL BARTENDER</h1>
         <div className="home">
           <div className="home-content">
-            {content}
+            {
+              content.split("\n").map((item, key) => (
+                <div key={key}>
+                  <div className="sentence">{item}</div>
+                </div>
+              ))
+            }
             <div className="search">
             </div>
           </div>
